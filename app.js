025329f8d73102fb2bfa602f1a187ff7e9cd750f
Michael Katan <michaelkatan@gmail.com>
2019-08-18 00:51:58 +0300

    ..

--- app.js
+++ app.js
@@ -30,6 +30,21 @@ let db = admin.firestore();
 
 var id = 0;
 
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 app.post('/addQuestion', (request, respond) =>
 {
   const title = request.get("title");
@@ -61,19 +76,6 @@ app.post('/addQuestion', (request, respond) =>
         respond.sendStatus(500).send(error)
       })
 });
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
