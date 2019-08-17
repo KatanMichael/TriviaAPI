47fa032e0b70e8b7a828de0768b410097c87610c
Michael Katan <michaelkatan@gmail.com>
2019-08-18 00:01:06 +0300

    firebase added

--- app.js
+++ app.js
@@ -7,7 +7,34 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var admin = require('firebase-admin');
+var firebase = require('firebase');
+var firebaseui = require('firebaseui');
+
 var app = express();
+
+process.env.GOOGLE_APPLICATION_CREDENTIALS = "TriviaAPI-8749187274a7.json";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBLR6Sx_UZh8W1doAkDD8IdH8MW2JT0Euw",
+  authDomain: "triviaapi-d1d92.firebaseapp.com",
+  databaseURL: "https://triviaapi-d1d92.firebaseio.com",
+  projectId: "triviaapi-d1d92",
+  storageBucket: "",
+  messagingSenderId: "169366846849",
+  appId: "1:169366846849:web:f6f7686449ffc153"
+};
+
+admin.initializeApp(firebaseConfig);
+
+let db = admin.db;
+
+app.post('/addQuestion', (request, response) => 
+{
+  
+
+
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
---
+++ public/javascripts/index.js
@@ -0,0 +1,16 @@
+function addQuestionToDatabse(title,correct,r1,r2,r3,cat)
+{
+    var url = "/addQuestion"
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "POST", url, true ); // false for synchronous request
+    xmlHttp.setRequestHeader("title",title);
+    xmlHttp.setRequestHeader("correct",correct);
+    xmlHttp.setRequestHeader("wrong1",r1);
+    xmlHttp.setRequestHeader("wrong2",r2);
+    xmlHttp.setRequestHeader("wrong3",r3);
+    xmlHttp.setRequestHeader("category",cat);
+    xmlHttp.send();
+    return xmlHttp.responseText;
+
+
+}
